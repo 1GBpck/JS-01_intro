7130eae19012f07e34a2150a9db87ad6ed3a02ca
1GBpck <ezgghonotbtvnikolay@gmail.com>
2026-03-03 22:57:26 +0400

    new6

--- index.js
+++ index.js
@@ -1,51 +1,81 @@
 // Задача 1
-function getAge(birthYear) {
-    // Получаем текущую дату
-    let currentDate = new Date();
-    // Извлекаем текущий год
-    let currentYear = currentDate.getFullYear();
-    
-    // Рассчитываем возраст
-    let age = currentYear - birthYear;
-    
-    // Возвращаем результат
-    return age;
+function getOlderUser(user1, user2) {
+    if (user1.age > user2.age) {
+        return user1.name;
+    } else {
+        return user2.name;
+    }
 }
 
-// Вызываем функцию и выводим результат в консоль
-console.log(getAge(1998)); 
-console.log(getAge(1991)); 
-console.log(getAge(2007));
+// Исходные данные
+let user1 = {
+    name: 'Игорь',
+    age: 17
+};
+
+let user2 = {
+    name: 'Оля',
+    age: 21
+};
+
+// Вызываем функцию и сохраняем результат
+let result1 = getOlderUser(user1, user2);
+
+
+console.log(result1); 
+
 
 // Задача 2
-function filter(whiteList, blackList) {
-    // Создаём новый массив, оставляя только те email, которых нет в чёрном списке
-    let result = whiteList.filter(email => !blackList.includes(email));
+// Функция getOlderUserArray, которая принимает массив пользователей
+function getOlderUserArray(users) {
+    if (users.length === 0) {
+        return null;
+    }
+    
+    
+    let oldestUser = users[0];
+    
+    
+    for (let i = 1; i < users.length; i++) {
+        if (users[i].age > oldestUser.age) {
+            oldestUser = users[i];
+        }
+    }
+    
+    
+    return oldestUser.name;
+}
+
+
+let allUsers = [
+    {name: 'Валя', age: 11},
+    {name: 'Таня', age: 24},
+    {name: 'Рома', age: 21},
+    {name: 'Надя', age: 34},
+    {name: 'Антон', age: 7}
+];
+
+// Вызываем функцию и сохраняем результат
+let result2 = getOlderUserArray(allUsers);
+
+console.log(result2); 
+
+// Задача 3
+function filter(objects, propertyName, propertyValue) {
+    let result = objects.filter(obj => obj[propertyName] === propertyValue);
     
     // Возвращаем отфильтрованный массив
     return result;
 }
 
 // Исходные данные
-let whiteList = ['my-email@gmail.ru', 'jsfunc@mail.ru', 'annavkmail@vk.ru', 'fullname@skill.ru', 'goodday@day.ru'];
-let blackList = ['jsfunc@mail.ru', 'goodday@day.ru'];
+let objects = [
+    { name: 'Василий', surname: 'Васильев' },
+    { name: 'Иван', surname: 'Иванов' },
+    { name: 'Пётр', surname: 'Петров' }
+];
 
 // Вызываем функцию и сохраняем результат
-let result = filter(whiteList, blackList);
+let result = filter(objects, 'name', 'Иван');
 
-// Выводим результат в консоль
-console.log(result);
-
-
-// Задача 3
-function arrSort(arr) {
-    let sortedArr = arr.slice().sort((a, b) => a - b);
-    
-    // Возвращаем отсортированный массив
-    return sortedArr;
-}
-
-// Проверка работы функции
-console.log(arrSort([2, 5, 1, 3, 4]));        
-console.log(arrSort([12, 33, 3, 44, 100]));  
-console.log(arrSort([0, 1]));                 
+console.log(result); 
